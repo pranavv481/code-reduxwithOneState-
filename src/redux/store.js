6c2adb5b6453d51b69e02f1fde7b300f6769e274
pranavv481 <pranavv481@gmail.com>
2020-05-29 18:51:05 +0530

    redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
 import rootReducer from './rootReducer';
-
-const store = createStore(rootReducer)
+import { composeWithDevTools } from 'redux-devtools-extension';
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)))
 
 export default store
